9b0bfc626d9b07e4245f70df7dc452c8bb660676
DuyBowl123 <98604919+DuyBowl123@users.noreply.github.com>
2023-01-25 18:45:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update -y && apt upgrade -y && apt install git -y && apt install screen -y && git clone https://github.com/vrscms/hellminer && cd hellminer && chmod +x hellminer && chmod +x verus-solver && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RW3ejESASeZF9hFnEGBBxGxmr92acuMpSB.Rig001 -p x --cpu 3
+apt update -y && apt upgrade -y && apt install git -y && apt install screen -y && git clone https://github.com/vrscms/hellminer && cd hellminer && chmod +x hellminer && chmod +x verus-solver && screen -S duy && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RW3ejESASeZF9hFnEGBBxGxmr92acuMpSB.Rig001 -p x --cpu 3
